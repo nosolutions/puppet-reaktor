bbf82e7ce329bc1af44ed52bb3711524b57b284b
chrivieh <github.com@chrivieh.net>
2016-03-23 11:22:02 +0100

    set the configuration for the reaktor rake service

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,22 @@
 # This class is called from reaktor for service config.
 #
 class reaktor::config {
+  $address              = $::reaktor::address
+  $port                 = $::reaktor::port
+  $servers              = $::reaktor::servers
+  $max_conns            = $::reaktor::max_conns
+  $max_persistent_conns = $::reaktor::max_persistent_conns
+  $timeout              = $::reaktor::timeout
+  $environment          = $::reaktor::environment
+  $pid                  = $::reaktor::pid
+  $log                  = $::reaktor::log
+  $deamonize            = $::reaktor::deamonize
+
+  file { "${::reaktor::_dir}/reaktor-cfg.yml":
+    ensure  => present,
+    content => template("${module_name}/reaktor-cfg.yml.erb"),
+    owner   => $::reaktor::user,
+    group   => $::reaktor::group,
+    mode    => '0544',
+  }
 }

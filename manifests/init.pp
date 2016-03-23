0bcf4d727ef3945da9b826215a4df5e9cedbe1ff
chrivieh <github.com@chrivieh.net>
2016-03-23 11:19:50 +0100

    move default params to from manifests/init.pp to manifests/params.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,28 +12,154 @@
 #   Reaktor runs under this user
 #   Default: 'reaktor'
 #
-# [*repo*]
-#   URL of reaktor repository to clone
-#   Default: 'https://github.com/pzim/reaktor.git',
+# [*homedir*]
+#   Defines the user's home directory which is also the directory where reaktor is installed to.
+#   Default: `/opt/reaktor`
+#
+# [*shell*]
+#   Defines the user's shell.
+#   Default: `/usr/sbin/nologin`
+#
+# [*uid*]
+#   Defines the user's UID.
+#   Default: `4500`
+#
+# [*manage_group*]
+#   Defines it the group is created or not.
+#   Default `true`.
+#
+# [*group*]
+#   Defines the group.
+#   Default: `reaktor`.
+#
+# [*gid*]
+#   Defines the group's GID.
+#   Default: `4500`.
+#
+# [*manage_service*]
+#   Defines if the service(s) to run reaktor are created or not.
+#   Default: `true` on Ubuntu, `false` on all other systems.
+#
+# [*service_provider*]
+#   A String defining the service provider.
+#   Default: `upstart` on Ubuntu, not set on all other systems.
+#
+# [*init_dir*]
+#   A string defining the init direcotry.
+#   Default: `/etc/init` on Ubunutu, not set on all other systems.
+#
+# [*dir*]
+#   A string defining the directory where reaktor is installed.
+#   Default `$reaktor::homedir`.
+#
+# [*repository*]
+#   Defines the repository of the reaktor source code.
+#   Default: `https://github.com/pzim/reaktor.git`.
+#
+# [*build_essentials_package*]
+#   Defines the package name for the build essentials.
+#   Default: `build-essential` for Ubuntu, `undef` for all other systems.
+#
+# [*config*]
+#   A hash defining the configuration for reaktor. See the reaktor doc for more information (https://github.com/pzim/reaktor#environment-variables). In case the service runs as a (Ubuntu/upstart) service the environment variables are inserted into the service script.
+#   Default: empty.
+#
+# [*address*]
+#   Rake config defining the address.
+#   Default: `localhost`.
+#
+# [*port*]
+#   Rake config defining the port.
+#   Default: `4570`.
+#
+# [*servers*]
+#   Rake config defining the number of servers.
+#   Default: `1`.
+#
+# [*max_conns*]
+#   Rake config defining the maximum number of connections.
+#   Default: `1024`.
+#
+# [*max_persistent_conns*]
+#   Rake config defining the maximum number of persistent connections.
+#   Default: `512`.
+#
+# [*timeout*]
+#   Rake config defining the timeout.
+#   Default: `30`.
+#
+# [*environment*]
+#   Rake config defining the environment.
+#   Default: `production`.
+#
+# [*pid*]
+#   Rake config defining the PID.
+#   Default: `tmp/pids/reaktor.pid`.
+#
+# [*log*]
+#   Rake config defining the rake log output.
+#   Default: `reaktor.log`.
+#
+# [*daemonize*]
+#   Rake config defining if reaktor runs as a daemon or not.
+#   Default: `false` on Ubuntu, `true` on all other systems.
+#   Needs to be set to false on Ubuntu if reaktor should run as an (upstart) service.
+
 class reaktor (
-  $manage_user  = true,
-  $user         = 'reaktor',
-  $homedir      = '/home/reaktor',
-  $uid          = 4500,
-  $gid          = 4500,
-  $repository   = 'https://github.com/pzim/reaktor.git',
+  $manage_user              = $::reaktor::params::manage_user,
+  $user                     = $::reaktor::params::user,
+  $homedir                  = $::reaktor::params::homedir,
+  $shell                    = $::reaktor::params::shell,
+  $uid                      = $::reaktor::params::uid,
+
+  $manage_group             = $::reaktor::params::manage_group,
+  $group                    = $::reaktor::params::group,
+  $gid                      = $::reaktor::params::gid,
+
+  $manage_service           = $::reaktor::params::manage_service,
+  $service_provider         = $::reaktor::params::service_provider,
+  $init_dir                 = $::reaktor::params::init_dir,
+
+  $dir                      = undef,
+  $repository               = $::reaktor::params::repository,
+  $build_essentials_package = $::reaktor::params::build_essentials_package,
+  $config                   = {},
+  
+  $address              = $::reaktor::params::address,
+  $port                 = $::reaktor::params::port,
+  $servers              = $::reaktor::params::servers,
+  $max_conns            = $::reaktor::params::max_conns,
+  $max_persistent_conns = $::reaktor::params::max_persistent_conns,
+  $timeout              = $::reaktor::params::timeout,
+  $environment          = $::reaktor::params::environment,
+  $pid                  = $::reaktor::params::pid,
+  $log                  = $::reaktor::params::log,
+  $daemonize            = $::reaktor::params::daemonize,
   ) inherits ::reaktor::params {
 
   # validate parameters here
   validate_bool($manage_user)
   validate_string($user)
+  validate_absolute_path($homedir)
+  validate_absolute_path($shell)
   validate_integer($uid)
+
+  validate_bool($manage_group)
+  validate_string($group)
   validate_integer($gid)
+
   validate_string($repository)
 
-  class { '::reaktor::install': }
-  # class { '::reaktor::config': } ~>
-  # class { '::reaktor::service': } ->
+  $_dir = $dir ? {
+    undef   => $homedir,
+    default => $dir
+  }
+  validate_absolute_path($_dir)
 
-  # Class['::reaktor']
+  class { '::reaktor::install': }
+
+  if $manage_service {
+    class { '::reaktor::service': }
+  }
+  class { '::reaktor::config': }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,14 +4,46 @@
 # It sets variables according to platform.
 #
 class reaktor::params {
+
+  $ensure       = 'present'
+
+  $manage_user  = true
+  $user         = 'reaktor'
+  $homedir      = '/opt/reaktor'
+  $shell        = '/usr/sbin/nologin'
+  $uid          = 4500
+
+  $manage_group = true
+  $group        = 'reaktor'
+  $gid          = 4500
+
+  $repository   = 'https://github.com/pzim/reaktor.git'
+
+  # reaktor configs
+  $address              = 'localhost'
+  $port                 = 4570
+  $servers              = 1
+  $max_conns            = 1024
+  $max_persistent_conns = 512
+  $timeout              = 30
+  $environment          = 'production'
+  $pid                  = 'tmp/pids/reaktor.pid'
+  $log                  = 'reaktor.log'
+
   case $::osfamily {
     'Debian': {
-      $package_name = 'reaktor'
-      $service_name = 'reaktor'
+      $manage_service           = true
+      $service_name             = 'reaktor'
+      $service_provider         = 'upstart'
+      $build_essentials_package = 'build-essential'
+      $init_dir                 = '/etc/init'
+      $daemonize                = false
     }
     'RedHat', 'Amazon': {
-      $package_name = 'reaktor'
-      $service_name = 'reaktor'
+      $manage_service           = false
+      $service_name             = 'reaktor'
+      $build_essentials_package = undef
+      $daemonize                = true
     }
     default: {
       fail("${::operatingsystem} not supported")
